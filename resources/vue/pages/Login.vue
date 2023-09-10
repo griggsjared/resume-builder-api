fd3bcbdd607fc81dc2189dc72790b088e1597055
Jared Griggs <griggsjared@gmail.com>
2023-09-10 14:57:00 -0600

    remove most front end functionality

--- resources/vue/pages/Login.vue
+++
@@ -1,8 +0,0 @@
-<script type="ts" setup>
-import { computed } from 'vue';
-const hello = computed(() => 'Hello World!!! And this is a computed property.');
-</script>
-
-<template>
-<p class="text-gray-700">{{ hello }}</p>
-</template>
