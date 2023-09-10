5fd965201995ff9a3dbd89547334fbfe5adee2a5
Jared Griggs <griggsjared@gmail.com>
2023-09-10 14:59:52 -0600

    remove graph graphql api and related

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite(['resources/scripts/app.ts'])
-    @inertiaHead
   </head>
-  <body class="font-sans leading-normal tracking-normal bg-gray-100">
-    @inertia
+  <body >
+    Resume Builder
   </body>
 </html>
