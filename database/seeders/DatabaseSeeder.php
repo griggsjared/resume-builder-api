0038fa9bfd2370320795e6c0aac0901eab5d2fb2
Jared Griggs <griggsjared@gmail.com>
2023-10-09 09:00:34 -0600

    update test data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Domains\Resumes\Models\Education;
+use App\Domains\Resumes\Models\EducationHighlight;
 use App\Domains\Resumes\Models\Employer;
 use App\Domains\Resumes\Models\EmployerHighlight;
 use App\Domains\Resumes\Models\Skill;
@@ -20,33 +21,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $email = 'text@example.com';
-        $password = 'password';
+        $email = 'test@example.com';
+        $password = 'P@ssw0rd';
 
         $user = User::factory()->admin()->create([
             'email' => $email,
             'password' => bcrypt($password),
         ]);
 
-        $token = $user->createToken('test-token');
-
-        Subject::factory(1)
-            ->for($user, 'user')
-            ->has(SubjectHighlight::factory(3), 'highlights')
-            ->has(Skill::factory(5)->uncategorized(), 'skills')
-            ->has(Skill::factory(20), 'skills')
-            ->has(
-                Employer::factory(1)->current()->has(EmployerHighlight::factory(3), 'highlights')
-            )
-            ->has(
-                Employer::factory(1)->has(EmployerHighlight::factory(3), 'highlights')
-            )
-            ->has(Education::factory(1), 'education')
-            ->create();
+        $token = explode('|', $user->createToken('test-token')->plainTextToken);
 
         $this->command->info('Test user created:');
         $this->command->info("Email: {$email}");
         $this->command->info("Password: {$password}");
-        $this->command->info("Access Token: {$token->plainTextToken}");
+        $this->command->info("Access Token: {$token[1]}");
+
+        User::factory(10)
+            ->basic()
+            ->has(
+                Subject::factory(rand(1, 3))
+                    ->has(SubjectHighlight::factory(rand(1, 3)), 'highlights')
+                    ->has(Skill::factory(rand(1, 2))->uncategorized(), 'skills')
+                    ->has(Skill::factory(rand(2, 8)), 'skills')
+                    ->has(
+                        Employer::factory(1)->current()->has(EmployerHighlight::factory(rand(1, 3)), 'highlights'),
+                        'employers'
+                    )
+                    ->has(
+                        Employer::factory(rand(2, 3))->has(EmployerHighlight::factory(rand(1, 3)), 'highlights'),
+                        'employers'
+                    )
+                    ->has(
+                        Education::factory(1)->current()->Has(EducationHighlight::factory(rand(1, 3)), 'highlights'),
+                        'education'
+                    )
+                    ->has(
+                        Education::factory(rand(2, 3))->has(EducationHighlight::factory(rand(1, 3)), 'highlights'),
+                        'education'
+                    ),
+                'subjects'
+            )
+            ->create();
     }
 }
